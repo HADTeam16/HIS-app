ea1c07cbfdab6e918d7add5a1ba4e295adc364c7
Abhinav Dhoka <abhinavdhoka@gmail.com>
2024-01-31 04:07:23 +0530

    animations updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,ts}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+    content: ["./src/**/*.{html,ts}"],
+    theme: {
+        extend: {
+            colors: {
+                // primary: "#673AB7",
+                // secondary: "#FFC107",
+                
+                primary: "#80CBC4",
+                secondary: "#F48FB1",
+            },
+        },
+    },
+    plugins: [],
+};
