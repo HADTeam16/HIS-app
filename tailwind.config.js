0608245537ca61eed2d74acc0a36b908df1fee92
Abhinav Dhoka <abhinavdhoka@gmail.com>
2024-02-01 02:24:05 +0530

    footer changed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+    corePlugins: {
+        preflight: false,
+    },
     content: ["./src/**/*.{html,ts}"],
     theme: {
         extend: {
             colors: {
                 // primary: "#673AB7",
                 // secondary: "#FFC107",
-                
-                primary: "#80CBC4",
-                secondary: "#F48FB1",
+
+                primary: "#009688",
+                secondary: "#F8FAE5",
+                tertiary: "#B19470",
+                quaternary: "#76453B",
             },
         },
     },
