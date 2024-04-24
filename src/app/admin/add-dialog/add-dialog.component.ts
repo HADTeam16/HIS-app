410bcde6d907c25a3e62c5c05493f3fbcbd9ac2e
shashankmittra <shashankmittra@Shashanks-MacBook-Air.local>
2024-04-24 14:54:32 +0530

    Modified Admin page styling wise

--- src/app/admin/add-dialog/add-dialog.component.ts
+++ src/app/admin/add-dialog/add-dialog.component.ts
@@ -3,20 +3,22 @@ import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
-  selector: 'app-add-dialog',
-  template: `
-    <!-- Add content specific to each tab -->
-    <ng-container [ngSwitch]="tabIndex">
-      <app-doctor-dialog *ngSwitchCase="0"></app-doctor-dialog>
-      <app-receptionist-dialog *ngSwitchCase="1"></app-receptionist-dialog>
-      <app-nurse-dialog *ngSwitchCase="2"></app-nurse-dialog>
-    </ng-container>
-  `,
+    selector: 'app-add-dialog',
+    template: `
+        <!-- Add content specific to each tab -->
+        <ng-container [ngSwitch]="tabIndex">
+            <app-doctor-dialog *ngSwitchCase="0"></app-doctor-dialog>
+            <app-receptionist-dialog
+                *ngSwitchCase="1"
+            ></app-receptionist-dialog>
+            <app-nurse-dialog *ngSwitchCase="2"></app-nurse-dialog>
+        </ng-container>
+    `,
 })
 export class AddDialogComponent {
-  constructor(@Inject(MAT_DIALOG_DATA) public data: { tabIndex: number }) {}
+    constructor(@Inject(MAT_DIALOG_DATA) public data: { tabIndex: number }) {}
 
-  get tabIndex(): number {
-    return this.data.tabIndex;
-  }
+    get tabIndex(): number {
+        return this.data.tabIndex;
+    }
 }
