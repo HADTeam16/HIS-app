2523386372d312712d8bf334611021137d9f9860
Abhinav Dhoka <abhinavdhoka@gmail.com>
2024-05-11 01:32:56 +0530

    bottom navbar added

--- src/app/doctor/doctor.component.ts
+++ src/app/doctor/doctor.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 import { AuthService } from '../shared/services/auth.service';
 import { SnackbarService } from '../material/services/snackbar.service';
 import { User } from '../shared/models/user';
-import { DomSanitizer } from '@angular/platform-browser';
+import { BreakpointService } from '../material/services/breakpoint.service';
+import { Subscription } from 'rxjs';
 
 @Component({
     selector: 'app-doctor',
@@ -17,17 +18,28 @@ export class DoctorComponent {
         { title: 'Statistics', icon: 'monitoring', link: 'stats' },
     ];
     user: User = new User();
+    isTablet: boolean;
+    bpsub: Subscription;
+
     constructor(
         private authService: AuthService,
         private snackbarService: SnackbarService,
+        private breakPointService: BreakpointService
     ) {
         this.authService.user.subscribe((res) => {
             if (res) this.user = res;
+        });
+        this.bpsub = this.breakPointService.isTablet.subscribe((res) => {
+            this.isTablet = res;
         });
     }
 
     onLogout() {
         this.authService.logout();
         this.snackbarService.openSnackBar('Logged out successfully');
+    }
+
+    ngOnDestroy() {
+        this.bpsub.unsubscribe();
     }
 }
